5f536e614c69c4abcad0625491a65309bab444bc
Matt Amponah <mattamponsah@gmail.com>
2020-09-04 02:35:16 +0000

    Fixed Fahrenheit to Celsius Conversion Factor

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 Temp1= float(input("Enter temperature:"))
  
-unit =input("Enter unit in F/f or C/c: ")
+unit =input(" Enter unit in F/f or C/c: ")
 
-tempc = float(Temp1*5.0/9.0-32.0)
+tempc = float((Temp1-32.0)*5.0/9.0)
 
 tempf = float(Temp1*9.0/5.0+32.0)
 
